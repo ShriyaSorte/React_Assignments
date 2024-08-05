7f184a47f1dbeecd7eff89c34a035234361b038d
Shriya Sorte <shriyasorte2000@gmail.com>
2024-08-05 16:34:02 +0530

    remaining questions

---
+++ assignment11, 12/src/components/Post1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Post1() {
+  return (
+    <div>
+      <h2>Post 1</h2>
+      <p>This is the content of Post 1.</p>
+    </div>
+  );
+}
+
+export default Post1;
---
+++ assignment11, 12/src/components/Post2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Post2() {
+  return (
+    <>
+      <h2>Post 2</h2>
+      <p>This is the content of Post 2.</p>
+    </>
+  );
+}
+
+export default Post2;
