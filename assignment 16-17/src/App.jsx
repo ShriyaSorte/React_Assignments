77f1d2bce2518b1e7fa9505c41cd3cd8261c20c8
Shriya Sorte <shriyasorte2000@gmail.com>
2024-08-09 16:25:46 +0530

    assignment 16-17

---
+++ assignment 16-17/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import ProductList from './customHooks/ProductList'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <ProductList/>
+    </>
+  )
+}
+
+export default App
